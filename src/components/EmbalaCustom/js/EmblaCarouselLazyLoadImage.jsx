adf202dab675d81fb09dd5762fd7407a9bdd3cfa
kanwar-sikandar-ali-soc <kanwar.sikandar@soc-solution.com>
2025-04-16 22:44:43 +0500

    Image performace lazy loading

--- src/components/EmbalaCustom/js/EmblaCarouselLazyLoadImage.jsx
+++ src/components/EmbalaCustom/js/EmblaCarouselLazyLoadImage.jsx
@@ -1,38 +1,40 @@
-import Image from 'next/image';
-import React, { useState, useCallback } from 'react';
+import Image from 'next/image'
+import React, { useState, useCallback } from 'react'
 
-const PLACEHOLDER_SRC = '/assets/images/fycht.jpg';
+const PLACEHOLDER_SRC = `/assets/images/fycht.jpg`
 
-export const LazyLoadImage = ({ imgSrc, inView }) => {
-  const [hasLoaded, setHasLoaded] = useState(false);
-  const [src, setSrc] = useState(imgSrc);
+export const LazyLoadImage = (props) => {
+  const { imgSrc, inView } = props
+  const [hasLoaded, setHasLoaded] = useState(false)
 
-  const handleLoad = useCallback(() => {
-    if (inView) setHasLoaded(true);
-  }, [inView]);
-
-  const handleError = useCallback(() => {
-    if (src !== PLACEHOLDER_SRC) {
-      setSrc(PLACEHOLDER_SRC);
-    }
-    setHasLoaded(true);
-  }, [src]);
+  const setLoaded = useCallback(() => {
+    if (inView) setHasLoaded(true)
+  }, [inView, setHasLoaded])
 
   return (
     <div className="embla__slide">
-      {!hasLoaded && <span className="embla__lazy-load__spinner" />}
-      <div className={`embla__lazy-load${hasLoaded ? ' embla__lazy-load--has-loaded' : ''}`}>
+        {!hasLoaded && <span className="embla__lazy-load__spinner" />}
+
+        {/* <span className="embla__lazy-load__spinner" /> */}
+
+      <div
+        className={'embla__lazy-load'.concat(
+          hasLoaded ? ' embla__lazy-load--has-loaded' : ''
+        )}
+      >
         <Image
           className="embla__slide__img embla__lazy-load__img"
-          onLoad={handleLoad}
-          onError={handleError}
-          src={inView ? src : PLACEHOLDER_SRC}
+          onLoad={setLoaded}
+          src={inView ? imgSrc : PLACEHOLDER_SRC}
           alt="loadAlt"
-          width={100} // Adjusted to maintain aspect ratio
           height={240}
-          style={{ width: '100%', height: '240px', objectFit: 'cover' }}
+          width={100}
+          data-src={imgSrc}
+          onError={(e) => {
+            e.target.src = '/assets/images/fycht.jpg';
+          }}
         />
       </div>
     </div>
-  );
-};
+  )
+}
